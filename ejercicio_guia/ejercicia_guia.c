e33e4aa5879276203eb6afde80de50dcf6829a77
IsmaelBenitez <ismael.benitez@estudiantat.upc.edu>
2021-10-24 19:45:54 +0200

    entrega ejercicio guia L2

--- ejercicio_guia/ejercicia_guia.c
+++ ejercicio_guia/ejercicia_guia.c
@@ -5,31 +5,23 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <stdio.h>
-int main(int argc, char *argv[]) {
-	int sock_conn, sock_listen, ret;
+#include <pthread.h>
+
+void *AtenderCliente(void *socket)
+{
+	int sock_conn, ret;
+	int *s;
+	s=(int *) socket;
+	sock_conn=*s;
 	struct sockaddr_in serv_adr;
 	char peticion[512];
 	char respuesta[512];
 	// INICIALITZACIONS
 	// Obrim el socket
-	if ((sock_listen = socket(AF_INET, SOCK_STREAM, 0)) < 0)
-		printf("Error creant socket");
-	memset(&serv_adr, 0, sizeof(serv_adr));// inicialitza a zero serv_addr
-	serv_adr.sin_family = AF_INET;
-	serv_adr.sin_addr.s_addr = htonl(INADDR_ANY);
-
-	serv_adr.sin_port = htons(9000);
-	if (bind(sock_listen, (struct sockaddr *) &serv_adr, sizeof(serv_adr)) < 0)
-		printf ("Error al bind");
-	if (listen(sock_listen, 3) < 0)
-		printf("Error en el Listen");
 	//Se acaba de iniiciar
 	for(;;){
 		
-		printf ("Escuchando\n");
 		
-		sock_conn = accept(sock_listen, NULL, NULL);
-		printf ("He recibido conexion\n");
 		
 		int terminar=0;
 		
@@ -73,6 +65,41 @@ int main(int argc, char *argv[]) {
 		
 	}
 	
-	return 0;
+	
+}
+int main(int argc, char *argv[])
+{
+	int sock_conn,sock_listen,ret;
+	struct sockaddr_in serv_adr;
+	char peticion[512];
+	char respuesta[512];
+	// INICIALITZACIONS
+	// Obrim el socket
+	if ((sock_listen = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+		printf("Error creant socket");
+	memset(&serv_adr, 0, sizeof(serv_adr));// inicialitza a zero serv_addr
+	serv_adr.sin_family = AF_INET;
+	serv_adr.sin_addr.s_addr = htonl(INADDR_ANY);
+	
+	serv_adr.sin_port = htons(9000);
+	if (bind(sock_listen, (struct sockaddr *) &serv_adr, sizeof(serv_adr)) < 0)
+		printf ("Error al bind");
+	if (listen(sock_listen, 3) < 0)
+		printf("Error en el Listen");
+	int sockets[100];
+	pthread_t thread[100];
+	int i=0;
+	for (;;)
+	{
+		printf("Escuchando\n");
+		
+		sock_conn=accept(sock_listen,NULL,NULL);
+		printf("He recibido conexion\n");
+		sockets[i]=sock_conn;
+		pthread_create(&thread[i],NULL,AtenderCliente,&sockets[i]);
+		i++;
+	}
+	
+	
 }
 
